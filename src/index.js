f901a13c74f1db2157f4c8b8f0212334b1382b3f
mmloveaa <mandychan1128@gmail.com>
2016-05-03 11:59:34 -0700

    updated the quotes

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@
  *
  * Examples:
  * One-shot model:
- *  User: "Alexa, ask Space Geek for a space fact"
- *  Alexa: "Here's your space fact: ..."
+ *  User: "Alexa, ask Space Geek for a space quote"
+ *  Alexa: "Here's your space quote: ..."
  */
 
 /**
@@ -24,22 +24,24 @@
 var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 /**
- * Array containing space facts.
+ * Array containing space quotes.
  */
-var SPACE_FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+var BURTON_QUOTE = [
+    "Worry about yourself",
+    "Stay Focus! Stay Focus now!",
+    "Kid, no excuse! You gotta Focus!!!",
+    "I have 24 hours in a day, kid. I don't sleep when i need to.",
+    "What the fork is wrong with my code! This is bullshit",
+    "Keep Calm and Make Today Productive",
+    "Talk dirty to me, Alexa",
+    "I am on a mission",
+    "Be productive",
+    "Do you have coke?",
+    "FYI, stop planning each day. Instead, plan on learning what you have on your plate and do not fucking move on to something else until you learn it",
+    "Alexa who's your daddy? Amazon",
+    "No Pussy here. Be a Man",
+    "I am so dirty. I need to wash my hair, brush my teeth, clean my dishes",
+    "I don't use my pinky to hold my laptop."
 ];
 
 /**
@@ -48,46 +50,46 @@ var SPACE_FACTS = [
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * SpaceGeek is a child of AlexaSkill.
+ * BurtonQuote is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var SpaceGeek = function () {
+var BurtonQuote = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-SpaceGeek.prototype = Object.create(AlexaSkill.prototype);
-SpaceGeek.prototype.constructor = SpaceGeek;
+BurtonQuote.prototype = Object.create(AlexaSkill.prototype);
+BurtonQuote.prototype.constructor = BurtonQuote;
 
-SpaceGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("SpaceGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
+BurtonQuote.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("BurtonQuote onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-SpaceGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("SpaceGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+BurtonQuote.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("BurtonQuote onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     handleNewFactRequest(response);
 };
 
 /**
  * Overridden to show that a subclass can override this function to teardown session state.
  */
-SpaceGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("SpaceGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
+BurtonQuote.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("BurtonQuote onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-SpaceGeek.prototype.intentHandlers = {
+BurtonQuote.prototype.intentHandlers = {
     "GetNewFactIntent": function (intent, session, response) {
         handleNewFactRequest(response);
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can ask Space Geek tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("You can ask Burton Quote tell me a quote, or, you can say exit... What can I help you with?", "What can I help you with?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -102,23 +104,22 @@ SpaceGeek.prototype.intentHandlers = {
 };
 
 /**
- * Gets a random new fact from the list and returns to the user.
+ * Gets a random new quote from the list and returns to the user.
  */
 function handleNewFactRequest(response) {
-    // Get a random space fact from the space facts list
-    var factIndex = Math.floor(Math.random() * SPACE_FACTS.length);
-    var fact = SPACE_FACTS[factIndex];
+    // Get a random space quote from the space quotes list
+    var quoteIndex = Math.floor(Math.random() * BURTON_QUOTE.length);
+    var quote = BURTON_QUOTE[quoteIndex];
 
     // Create speech output
-    var speechOutput = "Here's your space fact: " + fact;
+    var speechOutput = "Here's your Burton Quote: " + quote;
 
-    response.tellWithCard(speechOutput, "SpaceGeek", speechOutput);
+    response.tellWithCard(speechOutput, "BurtonQuote", speechOutput);
 }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the SpaceGeek skill.
-    var spaceGeek = new SpaceGeek();
-    spaceGeek.execute(event, context);
+    // Create an instance of the BurtonQuote skill.
+    var burtonQuote = new BurtonQuote();
+    burtonQuote.execute(event, context);
 };
-
